23ebb1866ce6a9ef19e5f06b83fb84f0acb746e1
Dancingtuna <paul9119298@gmail.com>
2024-02-13 21:12:18 +0900

    remove duplicate files

--- src/main/java/org/example/mariajeu/repository/WineRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.mariajeu.repository;
-
-import org.example.mariajeu.domain.Wine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WineRepository extends JpaRepository<Wine, Long> {
-}
