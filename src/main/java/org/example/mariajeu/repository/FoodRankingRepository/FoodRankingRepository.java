6e4c8d3f32294248e3c7d3919151d4f84e1edf2a
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-02-13 19:39:56 +0900

    Update FoodRankingRepository.java

--- src/main/java/org/example/mariajeu/repository/FoodRankingRepository/FoodRankingRepository.java
+++ src/main/java/org/example/mariajeu/repository/FoodRankingRepository/FoodRankingRepository.java
@@ -4,6 +4,7 @@ import org.example.mariajeu.domain.FoodArticle.FoodArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
 import java.util.List;
 
 public interface FoodRankingRepository extends JpaRepository<FoodArticle, Long> {
