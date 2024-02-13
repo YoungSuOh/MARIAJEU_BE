0752ec07d46bbc04567d627645eadf257c409fc8
Simzard10 <jysim0326@ajou.ac.kr>
2024-02-13 17:46:08 +0900

    테스트 코드

---
+++ src/test/java/org/example/mariajeu/service/RestaurantServiceTest.java
@@ -0,0 +1,20 @@
+package org.example.mariajeu.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantServiceTest {
+
+    @Test
+    void registerRestaurant() {
+    }
+
+    @Test
+    void getAllRestaurants() {
+    }
+
+    @Test
+    void getRestaurant() {
+    }
+}
