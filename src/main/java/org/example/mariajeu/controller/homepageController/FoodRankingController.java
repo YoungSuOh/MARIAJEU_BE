053dd88912beaeb2dffabaa72c848b2f94f0037d
wonseokyoon <01088115030a@gmail.com>
2024-02-14 14:11:04 +0900

    add feature exception handle

--- src/main/java/org/example/mariajeu/controller/homepageController/FoodRankingController.java
+++ src/main/java/org/example/mariajeu/controller/homepageController/FoodRankingController.java
@@ -1,13 +1,12 @@
 package org.example.mariajeu.controller.homepageController;
 
 import org.example.mariajeu.domain.homepageDomain.WineType;
+import org.example.mariajeu.dto.ErrorDTO;
 import org.example.mariajeu.dto.homepageDto.FoodRankingDTO;
 import org.example.mariajeu.service.homepageService.FoodRankingService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,21 +39,40 @@ public class FoodRankingController {
 
     @GetMapping("/views/wine")
     public ResponseEntity<List<FoodRankingDTO>> getFoodByViewsRankingByWineType(@RequestParam(required = false) List<WineType> wineTypes) {
+        validateWineTypes(wineTypes);
         return ResponseEntity.ok(foodRankingService.getFoodByViewsRankingByWineType(wineTypes));
     }
 
-
     @GetMapping("/comments/wine")
     public ResponseEntity<List<FoodRankingDTO>> getFoodByCommentsRankingByWineType(@RequestParam(required = false) List<WineType> wineTypes) {
+        validateWineTypes(wineTypes);
         return ResponseEntity.ok(foodRankingService.getFoodByCommentsRankingByWineType(wineTypes));
     }
 
     @GetMapping("/likes/wine")
     public ResponseEntity<List<FoodRankingDTO>> getFoodByLikesRankingByWineType(@RequestParam(required = false) List<WineType> wineTypes) {
+        validateWineTypes(wineTypes);
         return ResponseEntity.ok(foodRankingService.getFoodByLikesRankingByWineType(wineTypes));
     }
 
-    //예외처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorDTO> handleIllegalArgumentException(IllegalArgumentException ex) {
+        String errorMessage = "유효하지 않은 와인 타입";
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .errorStatus("INVALID_INPUT")
+                .errorContent(errorMessage)
+                .data(null)
+                .build();
+        return ResponseEntity.badRequest().body(errorDTO);
+    }
+
+    private void validateWineTypes(List<WineType> wineTypes) {
+        if (wineTypes == null || wineTypes.isEmpty()) {
+            throw new IllegalArgumentException("와인 타입을 입력하세요");
+        }
+    }
+
 
 
 }
