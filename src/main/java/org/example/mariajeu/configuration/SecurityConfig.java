f3e89481613fea917e7169f141e9e2212c30f418
YeomSB <stanley0927@naver.com>
2024-02-16 23:46:46 +0900

    레스토랑 기능 추가, cors 에러 수정, FoodArticle api 추가

--- src/main/java/org/example/mariajeu/configuration/SecurityConfig.java
+++ src/main/java/org/example/mariajeu/configuration/SecurityConfig.java
@@ -8,13 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -30,7 +35,8 @@ public class SecurityConfig {
 
 
         http
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+                .cors((Customizer<CorsConfigurer<HttpSecurity>>) corsConfiguration())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
@@ -54,5 +60,33 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    CorsConfiguration corsConfiguration() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.addAllowedOrigin("http://localhost:3000");
+
+        return configuration;
+    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        //허용할 url 설정
+//        configuration.addAllowedOrigin("http://localhost:3000");
+//        //허용할 헤더 설정
+//        configuration.addAllowedHeader("*");
+//        //허용할 http method
+//        configuration.addAllowedMethod("*");
+//        // 클라이언트가 접근 할 수 있는 서버 응답 헤더
+//        configuration.addExposedHeader(TokenProperties.AUTH_HEADER);
+//        configuration.addExposedHeader(TokenProperties.REFRESH_HEADER);
+//        //사용자 자격 증명이 지원되는지 여부
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//
+//    }
 
 }
