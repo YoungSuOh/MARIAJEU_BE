6817a1c302f9910bff94747a0e1035a8acde7195
YeomSB <stanley0927@naver.com>
2024-02-16 20:21:19 +0900

    jar

--- src/main/java/org/example/mariajeu/repository/boardRepository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.mariajeu.repository.boardRepository;
-
-import org.example.mariajeu.domain.boardDomain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
